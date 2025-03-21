c5a7bcce9e61881e0f165efd858be25ed515502b
Jordan Doublemart <jordan@doublemart.fr>
2025-03-21 11:10:54 +0100

    fix: update fields

--- src/handlers/validators/update-book.ts
+++ src/handlers/validators/update-book.ts
@@ -2,10 +2,20 @@ import Joi from "joi";
 
 export interface UpdateBookRequest {
   id: number;
+  name?: string;
+  author?: string;
+  numberOfVolumes?: number;
   price?: number;
+  rating?: number;
+  publicationDate?: Date;
 }
 
 export const BookUpdateValidation = Joi.object<UpdateBookRequest>({
   id: Joi.number().required(),
   price: Joi.number().min(1),
+  rating: Joi.number().min(1).max(5),
+  publicationDate: Joi.date(),
+  name: Joi.string(),
+  author: Joi.string(),
+  numberOfVolumes: Joi.number().integer().min(1),
 });
